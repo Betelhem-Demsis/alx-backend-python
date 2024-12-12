f398f6d49bf8e687184f71a5819edea5d6c534e2
betelhem demsis <bettydemsis@gmail.com>
2024-12-12 22:34:02 +0300

    modified

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -10,6 +10,8 @@ async def async_fetch_users(ALX_prodev):
             for row in results:
                 print(row)
     return 
+
+
 async def async_fetch_older_users(ALX_prodev):
     async with aiosqlite.connect(ALX_prodev) as db:
         async with db.execute("SELECT * FROM users WHERE age > ?", (40,)) as cursor:
@@ -17,7 +19,9 @@ async def async_fetch_older_users(ALX_prodev):
             print("Users older than 40:")
             for row in results:
                 print(row)
-    return  
+    return 
+
+
 async def fetch_concurrently():
     database_name = "ALX_prodev"
 
@@ -31,15 +35,17 @@ async def fetch_concurrently():
                 email TEXT NOT NULL
             )
         """)
+      
         await db.execute("INSERT INTO users (name, age, email) VALUES (?, ?, ?)", ("John Doe", 30, "john.doe@example.com"))
         await db.execute("INSERT INTO users (name, age, email) VALUES (?, ?, ?)", ("Jane Doe", 50, "jane.doe@example.com"))
         await db.commit()
 
+    
     await asyncio.gather(
-        async_fetch_users(database_name),
-        async_fetch_older_users(database_name)
+        async_fetch_users(database_name), 
+        async_fetch_older_users(database_name) 
     )
 
+
 if __name__ == "__main__":
- 
     asyncio.run(fetch_concurrently())
